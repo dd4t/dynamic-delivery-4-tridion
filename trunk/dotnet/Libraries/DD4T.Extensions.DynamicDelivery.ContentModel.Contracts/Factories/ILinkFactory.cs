18f3d88377d5cbe46d11d2998e2a971fd337862d
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2011-09-20 14:55:00 +0000

    Moving this to the 'Old' folder to keep it a while before throwing it out.

--- trunk/dotnet/Libraries/DD4T.Extensions.DynamicDelivery.ContentModel.Contracts/Factories/ILinkFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace DD4T.Extensions.DynamicDelivery.ContentModel.Factories
-{
-    public interface ILinkFactory
-    {
-        string ResolveLink(string componentUri);
-        string ResolveLink(string sourcePageUri, string componentUri, string excludeComponentTemplateUri);
-    }
-}
