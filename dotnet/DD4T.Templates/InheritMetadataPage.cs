7e0ab10a12bcfa31558d61619c52c4e24482de00
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2011-10-07 13:41:50 +0000

    Now metadata on the root structure group is also read

--- dotnet/DD4T.Templates/InheritMetadataPage.cs
+++ dotnet/DD4T.Templates/InheritMetadataPage.cs
@@ -8,6 +8,7 @@ using TCM = Tridion.ContentManager.ContentManagement;
 using Dynamic = DD4T.ContentModel;
 using DD4T.Templates.Base;
 using DD4T.Templates.Base.Builder;
+using DD4T.Templates.Base.Utils;
 
 namespace DD4T.Templates
 {
@@ -18,29 +19,46 @@ namespace DD4T.Templates
      
       protected override void TransformPage(Dynamic.Page page)
       {
+          GeneralUtils.TimedLog("start TransformPage with id " + page.Id);
+
          Page tcmPage = this.GetTcmPage();
          StructureGroup tcmSG = (StructureGroup)tcmPage.OrganizationalItem;
          
          String mergeActionStr = Package.GetValue("MergeAction");
          Dynamic.MergeAction mergeAction;
-         if (string.IsNullOrEmpty(mergeActionStr))
+         if (! string.IsNullOrEmpty(mergeActionStr))
          {
-            mergeAction = defaultMergeAction;
+             try
+             {
+                 mergeAction = (Dynamic.MergeAction)Enum.Parse(typeof(Dynamic.MergeAction), mergeActionStr);
+             }
+             catch
+             {
+                 GeneralUtils.TimedLog("unexpected merge action " + mergeActionStr + ", using default");
+                 mergeAction = defaultMergeAction;
+             }
          }
          else
          {
-            mergeAction = (Dynamic.MergeAction)Enum.Parse(typeof(Dynamic.MergeAction), mergeActionStr);
+             GeneralUtils.TimedLog("no merge action specified, using default");
+             mergeAction = defaultMergeAction;
          }
+         GeneralUtils.TimedLog("using merge action " + mergeAction.ToString());
 
-         while (tcmSG.OrganizationalItem != null)
+         while (tcmSG != null)
          {
+             GeneralUtils.TimedLog("found structure group with id " + tcmSG.Id);
+
             if (tcmSG.MetadataSchema != null)
             {
                TCM.Fields.ItemFields tcmFields = new TCM.Fields.ItemFields(tcmSG.Metadata, tcmSG.MetadataSchema);
+               GeneralUtils.TimedLog(string.Format("about to merge {0} fields on structure group with {1} fields on page ", tcmFields.Count, page.MetadataFields.Count));
+
                // change
                FieldsBuilder.AddFields(page.MetadataFields, tcmFields, 1, false, mergeAction, Manager);
+               GeneralUtils.TimedLog(string.Format("finished merging, we now have {0} fields on structure group and {1} fields on page ", tcmFields.Count, page.MetadataFields.Count));
             }
-            tcmSG = (StructureGroup)tcmSG.OrganizationalItem;
+            tcmSG = tcmSG.OrganizationalItem as StructureGroup;
          }
 
       }
