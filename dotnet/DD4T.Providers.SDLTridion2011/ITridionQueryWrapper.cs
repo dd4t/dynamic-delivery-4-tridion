a221637636ddad1bdbf95fe7b54b739a358349cc
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2011-10-27 11:38:52 +0000

    Extends IQuery interface (defined in contracts) Adds ToTridionQuery() method which returns a Tridion 2011 Query object

---
+++ dotnet/DD4T.Providers.SDLTridion2011/ITridionQueryWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tridion.ContentDelivery.DynamicContent.Query;
+
+namespace DD4T.Providers.SDLTridion2011
+{
+    public interface ITridionQueryWrapper : DD4T.ContentModel.Contracts.Providers.IQuery
+    {
+        Query ToTridionQuery();
+    }
+}
