d5944580a4fb951cbe8277681b8df284a22a907b
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2011-10-27 11:43:50 +0000

    Added IQuery as input parameter for IComponentProvider.FindComponents(IQuery)

--- dotnet/DD4T.ContentModel.Contracts/Factories/IComponentFactory.cs
+++ dotnet/DD4T.ContentModel.Contracts/Factories/IComponentFactory.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using DD4T.ContentModel.Contracts.Providers;
+using DD4T.ContentModel.Querying;
 
 namespace DD4T.ContentModel.Factories
 {
@@ -12,7 +13,7 @@ namespace DD4T.ContentModel.Factories
         bool TryGetComponent(string componentUri, out IComponent component);
         IComponent GetComponent(string componentUri);
         IList<IComponent> GetComponents(string[] componentUris);
-		IList<IComponent> FindComponents(ExtendedQueryParameters queryParameters);
+		IList<IComponent> FindComponents(IQuery queryParameters);
         IComponent GetIComponentObject(string componentStringContent);
     }
 }
