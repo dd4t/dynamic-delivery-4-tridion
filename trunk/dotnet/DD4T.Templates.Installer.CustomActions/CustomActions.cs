604e5b3bb9c7367253722074069b21ba3e76efde
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2012-03-03 00:01:57 +0000

    Added logic to installer to recycle the Tridion application pool

--- trunk/dotnet/DD4T.Templates.Installer.CustomActions/CustomActions.cs
+++ trunk/dotnet/DD4T.Templates.Installer.CustomActions/CustomActions.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+using Microsoft.Web.Administration;
 
 
 namespace DD4T.Templates.Installer.CustomActions
@@ -25,11 +26,70 @@ namespace DD4T.Templates.Installer.CustomActions
         {
             State = stateSaver;
             base.Install(stateSaver);
+            RecycleTridionAppPool(); // Note: I added this but it doesn't seem to work yet..
             UploadTemplatesToTridion();
 
         }
 
         #region CustomCode
+
+        private static string TridionWebPath
+        {
+            get
+            {
+                return string.Format("{0}\\Web\\", Tridion.ContentManager.ConfigurationSettings.GetTcmHomeDirectory());
+            }
+        }
+
+        private static string FindAppPoolName()
+        {
+            ServerManager serverManager = new ServerManager();
+            foreach (Site s in serverManager.Sites)
+            {
+                //Console.WriteLine("Site {0}", s.Name);
+
+                foreach (Application app in s.Applications)
+                {
+                    //Console.WriteLine("\tApplication: {0}", app.Path);
+
+                    foreach (VirtualDirectory virtDir in app.VirtualDirectories)
+                    {
+                        //Console.WriteLine("\t\tVirtual Dir: {0}", virtDir.Path);
+                        //Console.WriteLine("\tPhysical path: {0}", virtDir.PhysicalPath);
+                        if (Path.GetFullPath(virtDir.PhysicalPath) == Path.GetFullPath(TridionWebPath))
+                        {
+                            //Console.WriteLine("found Tridion web site");
+                            return app.ApplicationPoolName;
+                        }
+                    }
+                }
+            }
+            return "NOTFOUND";
+        }
+
+        private static void RecycleTridionAppPool()
+        {
+            ServerManager serverManager = new ServerManager();
+            string appPoolName = FindAppPoolName();
+            ApplicationPoolCollection applicationPoolCollection = serverManager.ApplicationPools;
+            foreach (ApplicationPool applicationPool in applicationPoolCollection)
+            {
+                //Console.WriteLine("found app pool {0}, comparing to Tridion app pool {1}", applicationPool.Name, appPoolName);
+                if (applicationPool.Name == appPoolName)
+                {
+                    //Console.WriteLine("found Tridion app pool, recycling it now...");
+                    // If the applicationPool is stopped, restart it.
+                    //Console.WriteLine("status: {0}", applicationPool.State);
+                    applicationPool.Recycle();
+                    //Console.WriteLine("status: {0}", applicationPool.State);
+                    System.Threading.Thread.Sleep(1000);
+                    //Console.WriteLine("status: {0}", applicationPool.State);
+                }
+            }
+        }
+
+    
+
         protected void UploadTemplatesToTridion()
         {
             const string TemplatesDllName = "DD4T.Templates.dll";
