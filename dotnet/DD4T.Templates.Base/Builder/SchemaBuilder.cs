964a8e346b3e50976c53a8d865a4253a75a3be51
rogier.oudshoorn@gmail.com <rogier.oudshoorn@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2012-11-21 15:27:48 +0000

    Merge of SKF-specific fixes to enable the merge

--- dotnet/DD4T.Templates.Base/Builder/SchemaBuilder.cs
+++ dotnet/DD4T.Templates.Base/Builder/SchemaBuilder.cs
@@ -17,7 +17,17 @@ namespace DD4T.Templates.Base.Builder
 			s.Id = tcmSchema.Id.ToString();
             s.Folder = manager.BuildOrganizationalItem((TCM.Folder)tcmSchema.OrganizationalItem);
             s.Publication = manager.BuildPublication(tcmSchema.ContextRepository);
-            s.RootElementName = tcmSchema.RootElementName;
+
+            if (!String.IsNullOrEmpty(tcmSchema.RootElementName))
+            {
+                s.RootElementName = tcmSchema.RootElementName;
+            }
+            // note that non-webschemas and multimedia schema's lack a root element. In order not
+            // to break deserialization, setting it to undefined will do
+            else
+            {
+                s.RootElementName = "undefined";
+            }
 
 			return s;
 		}
