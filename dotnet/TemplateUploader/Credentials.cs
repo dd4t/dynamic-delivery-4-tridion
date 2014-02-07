ec61488bfa732e940a39fc6e196963babda6f119
quirijn.slings@gmail.com <quirijn.slings@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2014-02-07 13:33:28 +0000

    Added support for multimedia components whose filename has no extension

---
+++ dotnet/TemplateUploader/Credentials.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TemplateUploader
+{
+    public partial class Credentials : Form
+    {
+        Main mainForm = null;
+        public Credentials(Main f)
+        {
+            mainForm = f;
+            InitializeComponent();
+        }
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            mainForm.Username = tbUsername.Text;
+            mainForm.Password = tbPassword.Text;
+            this.Close();
+            mainForm.ConnectToTridion();
+        }
+
+    }
+}
---
+++ dotnet/TemplateUploader/Main.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TemplateUploader
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL of the Tridion Content Manager";
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(223, 18);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(364, 20);
+            this.tbUrl.TabIndex = 1;
+            this.tbUrl.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbUrl_TextKeyUp);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(471, 103);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(251, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "URI of folder where DD4T templates must be stored";
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(280, 65);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(298, 20);
+            this.tbFolder.TabIndex = 2;
+            this.tbFolder.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbUrl_TextKeyUp);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnConnect);
+            this.panel2.Controls.Add(this.tbUrl);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbFolder);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(606, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(594, 138);
+            this.panel2.TabIndex = 4;
+
+
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 138);
+            this.panel1.TabIndex = 4;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 216);
+            this.Controls.Add(this.panel1);
+            this.Name = "Main";
+            this.Text = "DD4T Template Uploader";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
