5b005ad07ce962a2a4e728173505a848b8abbbcb
s990330@gmail.com <s990330@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2014-04-04 07:16:43 +0000

    --> Added 2013SP1 providers --> .NET versie from 2013sp1 providers is set to .4.5.1

---
+++ dotnet/DD4T.Providers.SDLTridion2013sp1/ITridionQueryWrapper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Tridion.ContentDelivery.DynamicContent.Query;
+using DD4T.ContentModel.Querying;
+
+namespace DD4T.Providers.SDLTridion2013sp1
+{
+    public interface ITridionQueryWrapper : IQuery
+    {
+        Query ToTridionQuery();
+    }
+}
