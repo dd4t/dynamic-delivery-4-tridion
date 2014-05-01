99e17f59eac1c969f25b5dbd5eb9664e3b456cf6
s990330@gmail.com <s990330@gmail.com@d02804ee-8c97-3c69-7a2e-97ba008867c9>
2014-05-01 12:31:56 +0000

    --> Fixed XmlMinimizer. Somehow this got overwritten with an old version.

--- dotnet/DD4T.Templates.Base/Utils/XmlMinimizer.cs
+++ dotnet/DD4T.Templates.Base/Utils/XmlMinimizer.cs
@@ -11,7 +11,7 @@ namespace DD4T.Templates.Base.Utils
 {
     public class XmlMinimizer
     {
-        public static string Convert(string input)
+        public static string Convert(string input, string minimizeSettings)
         {
 
             //load the Xml doc
@@ -27,8 +27,21 @@ namespace DD4T.Templates.Base.Utils
             //create the output stream 
             MemoryStream outputStream = new MemoryStream();
 
-            //do the actual transform of Xml
-            xslTransformer.Transform(myXPathDoc, null, outputStream);
+            //set up parameters and do the actual transform of Xml
+
+            if (string.IsNullOrEmpty(minimizeSettings))
+            {
+                xslTransformer.Transform(myXPathDoc, null, outputStream);
+            }
+            else
+            {
+                XsltArgumentList argsList = new XsltArgumentList();
+                if (minimizeSettings.ToLower().Contains("publication"))
+                    argsList.AddParam("remove-publications", "", true);
+                if (minimizeSettings.ToLower().Contains("folder"))
+                    argsList.AddParam("remove-folders", "", true);
+                xslTransformer.Transform(myXPathDoc, argsList, outputStream);
+            }
 
             // read output into a string
             outputStream.Seek(0, SeekOrigin.Begin);
